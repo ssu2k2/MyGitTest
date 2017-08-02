397e4ba2771cc0b87b32651a81a83948790252c7
yongsucho <yongsucho@gmail.com>
2017-08-02 14:58:04 +0900

    Add View

--- app/src/main/java/com/example/yongsu/mygittest/MainActivity.java
+++ app/src/main/java/com/example/yongsu/mygittest/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.yongsu.mygittest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = getClass().getSimpleName();
+    TextView tvTitle;
+    ImageView ivImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate()");
+
     }
 }
