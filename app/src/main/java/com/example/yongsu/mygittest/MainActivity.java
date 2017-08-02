9892799645067ddf6d4306b32daead637ea8323e
yongsucho <yongsucho@gmail.com>
2017-08-02 15:25:45 +0900

    test function

--- app/src/main/java/com/example/yongsu/mygittest/MainActivity.java
+++ app/src/main/java/com/example/yongsu/mygittest/MainActivity.java
@@ -16,10 +16,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate()");
-
+        test();
     }
     private void test() {
-
+        Log.d(TAG, "Call Test()");
     }
 
 }
