5f5c5715506b9c13b17c5034ebe217a3f6e9a0af
yongsucho <yongsucho@gmail.com>
2017-08-02 14:50:25 +0900

    First commit

--- app/src/main/java/com/example/yongsu/mygittest/MainActivity.java
+++ app/src/main/java/com/example/yongsu/mygittest/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.yongsu.mygittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final String TAG = getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate()");
     }
 }
